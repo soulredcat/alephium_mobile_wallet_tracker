9a903852f9c626595b005be3ebc3e2e16034f951
RED CAT <arifsofyankusumadiarto@gmail.com>
2026-08-10 11:30:58 +0700

    test: import material widgets for shell deactivation coverage

--- test/explorer_shell_lifecycle_test.dart
+++ test/explorer_shell_lifecycle_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
